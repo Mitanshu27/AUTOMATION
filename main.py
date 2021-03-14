c546f132e7b5ed2a306a498509dc7efa452b4bbf
Mitanshu Shah <61775352+Mitanshu27@users.noreply.github.com>
2021-03-14 17:19:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+search = input('Enter aplication name:- ')
+if(search == "zoom" or "Zoom" or "ZOOM"):
+    print("Opening Zoom Aplication")
+    os.system(r'C:\Users\Mitanshu\AppData\Roaming\Zoom\bin\Zoom.exe')
